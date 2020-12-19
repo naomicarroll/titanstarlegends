a76838e84d0adae24581527586b9627f37bb47e3
Naomi Carroll <naomi.j.carroll@gmail.com>
2020-12-19 16:03:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+// import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="talent-tree">
+          <div className="talent-path-container">
+            <div className="talent-path">
+              <button className="talent-button"></button>
+              <button className="talent-button"></button>
+              <button className="talent-button"></button>
+              <button className="talent-button"></button>
+            </div>
+
+            <div className="talent-path">
+              <button className="talent-button"></button>
+              <button className="talent-button"></button>
+              <button className="talent-button"></button>
+              <button className="talent-button"></button>
+            </div>
+          </div>
+          <div className="talent-point-counter">6/6</div>
+        </div>
       </header>
     </div>
   );
